459a6d963c42538f21452d1e44f893e48a8e2fa7
Coihue555 <kevlar555@gmail.com>
2022-03-08 18:13:42 -0300

    custombottomnavbar

--- disenos/lib/screens/home_screen.dart
+++ disenos/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:disenos/widgets/background.dart';
+import 'package:disenos/widgets/page_title.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,10 +10,28 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: const [
+        children:  [
           Background(),
+          _HomeBody(),
+
+
         ],
         )
+        //bottomNavigationBar: ,
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          PageTitle()
+        ],
+      ),
     );
   }
 }
