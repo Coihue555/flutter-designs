ce0c45dc26f6b04daf4ef1c1bbc052c199ccd0ca
Coihue555 <kevlar555@gmail.com>
2022-03-08 10:36:52 -0300

    Base

---
+++ disenos/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: TempHomeScreen()
+    );
+  }
+}
+
+class TempHomeScreen extends StatelessWidget {
+  const TempHomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Text('data'),
+        Text('otra'),
+      ],
+    );
+  }
+}
