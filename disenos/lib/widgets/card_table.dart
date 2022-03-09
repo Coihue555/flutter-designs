bb7b2f38c8f69a553e18940a1a42d183f9ed200b
Coihue555 <kevlar555@gmail.com>
2022-03-09 08:43:50 -0300

    final

--- disenos/lib/widgets/card_table.dart
+++ disenos/lib/widgets/card_table.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 
@@ -10,8 +12,38 @@ class CardTable extends StatelessWidget {
       children: const [
         TableRow(
           children: [
-            _SingleCard(icon: Icons.dashboard, color: Colors.orange, text: 'General',),
-            _SingleCard(icon: Icons.bus_alert, color: Colors.purple, text: 'Transports',),
+            _SingleCard(icon: Icons.dashboard, color: Colors.blueAccent, text: 'General',),
+            _SingleCard(icon: Icons.directions_bus_filled_outlined, color: Color.fromARGB(255, 178, 50, 201), text: 'Transports',),
+          ]
+        ),
+        TableRow(
+          children: [
+            _SingleCard(icon: Icons.local_grocery_store, color: Color.fromARGB(255, 247, 103, 151), text: 'Shopping',),
+            _SingleCard(icon: Icons.receipt, color: Color.fromARGB(255, 255, 139, 7), text: 'Bills',),
+          ]
+        ),
+        TableRow(
+          children: [
+            _SingleCard(icon: Icons.movie, color: Color.fromARGB(255, 93, 132, 214), text: 'Entertainment',),
+            _SingleCard(icon: Icons.fastfood, color: Color.fromARGB(255, 76, 184, 85), text: 'Grocery',),
+          ]
+        ),
+        TableRow(
+          children: [
+            _SingleCard(icon: Icons.dashboard, color: Colors.blueAccent, text: 'General',),
+            _SingleCard(icon: Icons.directions_bus_filled_outlined, color: Color.fromARGB(255, 178, 50, 201), text: 'Transports',),
+          ]
+        ),
+        TableRow(
+          children: [
+            _SingleCard(icon: Icons.local_grocery_store, color: Color.fromARGB(255, 247, 103, 151), text: 'Shopping',),
+            _SingleCard(icon: Icons.receipt, color: Color.fromARGB(255, 255, 139, 7), text: 'Bills',),
+          ]
+        ),
+        TableRow(
+          children: [
+            _SingleCard(icon: Icons.movie, color: Color.fromARGB(255, 93, 132, 214), text: 'Entertainment',),
+            _SingleCard(icon: Icons.fastfood, color: Color.fromARGB(255, 76, 184, 85), text: 'Grocery',),
           ]
         ),
 
@@ -34,27 +66,50 @@ class _SingleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var column = Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 5,),
+                CircleAvatar(
+                  backgroundColor: this.color,
+                  child: Icon(this.icon, size: 40, color: Colors.white,),
+                  radius: 48,
+                ),
+                const SizedBox(height: 5,),
+                Text(this.text, style: TextStyle(color: this.color, fontSize: 18),),
+        
+        
+              ]);
+    return _CardBackground(child: column);
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  final Widget child;
+
+  const _CardBackground({
+    Key? key,
+    required this.child
+    }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(62, 67, 107, 0.7),
-        borderRadius: BorderRadius.circular(20)
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          SizedBox(height: 5,),
-          CircleAvatar(
-            backgroundColor: Colors.blue,
-            child: Icon(Icons.pie_chart_outline, size: 80,),
-            radius: 48,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            height: 180,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(62, 67, 107, 0.7),
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: this.child,
           ),
-          SizedBox(height: 5,),
-          Text('General', style: TextStyle(color: Colors.blue, fontSize: 18),),
-
-
-        ]),
+        ),
+      ),
     );
   }
 }
